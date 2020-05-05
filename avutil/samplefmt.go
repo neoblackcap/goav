ff04a6db06f5125989cb3e7509694d371a311fd7
Tetsuyuki Kobayashi <tetsu.koba@gmail.com>
2020-05-05 16:42:09 +0900

    Add AvSampleAllocArrayAndSamples()

--- avutil/samplefmt.go
+++ avutil/samplefmt.go
@@ -3,6 +3,9 @@ package avutil
 //#cgo pkg-config: libavutil
 //#include <libavutil/samplefmt.h>
 import "C"
+import (
+	"unsafe"
+)
 
 const (
 	AV_SAMPLE_FMT_NONE = int(C.AV_SAMPLE_FMT_NONE)
@@ -25,4 +28,19 @@ const (
 
 func AvGetSampleFmtName(sampleFmt int) string {
 	return C.GoString(C.av_get_sample_fmt_name((C.enum_AVSampleFormat)(sampleFmt)))
+}
+
+func AvSamplesAllocArrayAndSamples(nbChannels, nbSamples int, sampleFmt int, align int) (ret int, audioData **uint8, linesize int) {
+	var linesize32 int32
+	ret = (int)(C.av_samples_alloc_array_and_samples(
+		(***C.uint8_t)(unsafe.Pointer(&audioData)),
+		(*C.int)(&linesize32),
+		C.int(nbChannels), C.int(nbSamples),
+		C.enum_AVSampleFormat(sampleFmt), C.int(align)))
+	return ret, audioData, int(linesize32)
+}
+
+func AvSamplesFreeArrayAndSamples(audioData **uint8) {
+	C.av_freep(unsafe.Pointer(audioData))
+	C.av_free(unsafe.Pointer(audioData))
 }
